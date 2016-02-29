1017ff3bb050d7d8a8c73133ec422aad03cff8b7
Guijin Ding <dingguijin@gmail.com>
2016-02-29 17:56:32 +0800

    check node/npm

--- ppmessage/scripts/require.py
+++ ppmessage/scripts/require.py
@@ -51,8 +51,7 @@ def _check_brew():
             continue
         brew_list.append(brew_item)
 
-    
-    should_list = ["ant", "boost", "cmake", "dos2unix", "faac",
+    should_list = ["ant", "boost", "cmake", "dos2unix", "faac", "node",
                    "fdk-aac", "ffmpeg", "gettext", "jpeg", "lame", "libffi", "libmagic",
                    "libogg", "libvo-aacenc", "libvorbis", "makedepend", "mercurial", "mp3val",
                    "mysql", "nginx-full", "opencore-amr", "openssl", "opus", "ossp-uuid", "pcre",
@@ -67,6 +66,28 @@ def _check_brew():
     return
 
 def _check_debian():
+    linux_distribution = platform.linux_distribution()
+    if linux_distribution[0] != "Debian" and linux_distribution[0] != "Ubuntu":
+        _color_print("not Debian or Ubuntu, ppmessage needs Debian or Ubuntu")
+        sys.exit()
+
+    which_cmd = "which dpkg"
+    try:
+        cmd_res = subprocess.check_output(which_cmd, shell=True)
+    except:
+        _color_print("no dpkg command installed, try `aptitude install dpkg`")
+        sys.exit()
+
+    dpkg_list = ["apt-file", "git", "mysql-server", "pkg-config", "nginx-extras", "python-pip", "wget", "gcc", "libffi-dev", "python-dev", "redis-server", "libjpeg8-dev", "libpng12-dev", "libfreetype6-dev", "mercurial", "g++", "libopencore-amrwb-dev", "libopencore-amrnb-dev", "libmp3lame-dev", "libopus-dev", "libmagic", "libpcre3", "libpcre3-dev", "openssl", "libssl-dev"]
+    
+    for dpkg_item in dpkg_list:
+        dpkg_cmd = "dpkg -s " + dpkg_item
+        try:
+            subprocess.check_output(dpkg_cmd, shell=True)
+        except:
+            _color_print("%s not installed, try `aptitude install %s`" % (dpkg_item, dpkg_item))
+            sys.exit()
+
     return
 
 def _require_ppmessage_pth():
@@ -143,17 +164,18 @@ def _require_pips():
             continue
         pip_list.append(pip_item.split(" ")[0])
 
-    should_list = ["apns-client", "AxmlParserPY", "beautifulsoup4",
-                   "biplist", "certifi", "cffi", "chardet", "cryptography", "evernote",
-                   "filemagic", "geoip2", "green", "identicon", "ipaddr", "ipython",
-                   "jieba", "matplotlib", "maxminddb", "mysql-connector-python", "numpy",
+    should_list = ["AxmlParserPY", "beautifulsoup4", "biplist", "certifi", "cffi", "chardet",
+                   "cryptography", "evernote", "filemagic", "geoip2", "green",
+                   "identicon", "ipaddr", "ipython", "jieba", "matplotlib",
+                   "maxminddb", "mysql-connector-python", "numpy",
                    "paho-mqtt", "paramiko", "Pillow", "pip", "ppmessage-mqtt", "pyOpenSSL",
                    "pyparsing", "pypinyin", "python-dateutil", "python-gcm", "qiniu", "qrcode",
                    "readline", "redis", "requests", "rq", "scikit-learn", "scipy", "setuptools",
                    "six", "SQLAlchemy", "supervisor", "tornado", "xlrd"]
+
     for should_item in should_list:
         if should_item not in pip_list:
-            _color_print("%s not installed, try use root privilege execute `pip install %s`" % should_item)
+            _color_print("%s not installed, try use root privilege execute `pip install %s`" % (should_item, should_item))
             sys.exit()
 
     _color_print("pip check ok!", "green")
@@ -163,6 +185,13 @@ def _require_node():
     """
     check node.js
     """
+    which_cmd = "which npm"
+    try:
+        subprocess.check_output(which_cmd, shell=True)
+    except:
+        _color_print("npm not installed, try use root privilege to install node.js")
+        sys.exit()
+
     return
 
 def _require():
