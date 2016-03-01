32be262c51a3ca60ea7a7ffc63b21488b8321232
Guijin Ding <dingguijin@gmail.com>
2016-03-01 16:42:21 +0800

    install sources

--- ppmessage/scripts/install_sources_mac.sh
+++ ppmessage/scripts/install_sources_mac.sh
@@ -5,3 +5,8 @@ git clone --recursive https://github.com/maxmind/libmaxminddb
 # autoconf libtool required to bootstrap
 cd libmaxminddb; ./bootstrap; ./configure; make; make install; cd -
 
+wget -c http://dev.mysql.com/get/Downloads/Connector-Python/mysql-connector-python-2.1.3.tar.gz
+tar zxvf mysql-connector-python-2.1.3.tar.gz
+cd mysql-connector-python-2.1.3
+sudo -H python setup.py install
+
