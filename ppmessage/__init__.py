ea248f7cd8a96d37ef9626eb18bb3e9fbf09cf05
Guijin Ding <dingguijin@gmail.com>
2016-03-11 11:13:00 +0800

    version 2.0.0.1 PPCom with PPKefu

--- ppmessage/__init__.py
+++ ppmessage/__init__.py
@@ -1,3 +1,18 @@
 import backend
 
-__version__ = "1.1.0.2"
+"""
+version format, MAIN.SUB.FEATURE.FIX
+
+1.0.0.0:
+Initial to SASS for ppmessage.cn
+
+2.0.0.0:
+Github to SASS for ppmessage.com
+
+2.0.0.1:
+PPCom send<->recv with PPKefu
+
+
+"""
+
+__version__ = "2.0.0.1"
